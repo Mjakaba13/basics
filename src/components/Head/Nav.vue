7dd5032ac9893beb3a9d098dcc2a02dc1e3c0b30
Mjakaba13 <malcolmakaba@gmail.com>
2022-07-01 14:38:35 +0000

    Hamburger done

--- src/components/Head/Nav.vue
+++ src/components/Head/Nav.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="meat">
-        <div>
+        <div id="gun">
             <img src="@/assets/logo-mob.svg" alt="">
         </div>
-        <button id="ham"><img src="@/assets/Ham-mob.svg" alt=""></button>
+        <button id="ham" v-on:click="displayMenu"><img class="open" :class="[toggle ? open2 : '']"
+                src="@/assets/Ham-mob.svg" alt=""><img :class="[toggle ? close2 : '']" class="close"
+                src="@/assets/closeicon.svg"></button>
     </div>
-    <div id="nice">
-        <div>
+    <div class="nice" :class="[toggle ? linkhidder : '']">
+        <div id="nices">
             <img src="@/assets/Logo.svg" alt="">
         </div>
         <div id="nicer">
@@ -22,11 +24,13 @@
             </ul>
             <but1 ButtonText="Register" id="but1"></but1>
         </div>
+        <div class="overlay"></div>
     </div>
 </template>
 
 <script>
 import but1 from "@/components/Button.vue"
+
 export default {
     name: "Nav-bar",
     components: { but1 },
@@ -38,7 +42,18 @@ export default {
                 { id: "l3", iden: "Pricing" },
                 { id: "l4", iden: "Solution" },
                 { id: "l5", iden: "Features" }
-            ]
+            ],
+            toggle: false,
+            close2: 'close2',
+            open2: 'open2',
+            linkhidder: 'linkhidder'
+
+        }
+    },
+    methods: {
+        displayMenu() {
+            this.toggle = !this.toggle
+
         }
     }
 }
@@ -49,7 +64,7 @@ export default {
     display: none;
 }
 
-#nice {
+.nice {
     display: flex;
     align-items: center;
     padding: 38px 60px;
@@ -97,14 +112,78 @@ li a {
         justify-content: space-between;
     }
 
-    #nice {
+    #gun {
+        z-index: 3;
+
+    }
+
+    #nices {
         display: none;
     }
 
     #ham {
         background-color: black;
         border: none;
+        /* right: 18px;
+        position: absolute; */
+        z-index: 2;
     }
 
+    .close {
+        display: none;
+
+    }
+
+    .close2 {
+        display: block;
+    }
+
+    .open2 {
+        display: none;
+    }
+
+    .nice {
+        display: none;
+    }
+
+    #nicer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0;
+
+    }
+
+    #nicer ul {
+        align-items: center;
+    }
+
+    #nicer ul,
+    #niced {
+        flex-direction: column;
+    }
+
+    #niced {
+        margin-top: 40px;
+    }
+
+    .linkhidder {
+        display: block;
+        position: absolute;
+        z-index: 1;
+        background-color: black;
+        width: 100%;
+    }
+
+    .overlay {
+        height: 100%;
+        width: 100%;
+        position: fixed;
+        background-color: black;
+        opacity: 0.5;
+        bottom: 0;
+        z-index: -1;
+        left: 0;
+    }
 }
 </style>
